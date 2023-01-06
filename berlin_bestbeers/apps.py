037fa3d067b5556e7377b835ee82696a37f6f3f4
vivignutz <vgnutzmann@gmail.com>
2023-01-06 16:27:49 +0000

    Add change name at manage.py

--- berlin_bestbeers/apps.py
+++ berlin_bestbeers/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BerlinBestbeersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BERLIN_BESTBEERS'
+    name = 'berlin_bestbeers'
