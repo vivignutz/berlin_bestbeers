2f2cd2d8c42ea26e09d08689472b8c2588eaf730
vivignutz <vgnutzmann@gmail.com>
2023-01-09 13:52:44 +0000

    Add templates .html

--- berlin_bestbeers/views.py
+++ berlin_bestbeers/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import generic # immpost django lib
+from .models import Post # import post views
 
-# Create your views here.
+#class RecentPostList(generic.ListView):
+#    """
+#    To display the 3 last posts in created date order
+#    """
+#    model = Post
+#    queryset = Post.objects.filter(status=1).order_by('-created_on')[:3]
+#    template_name = 'index.html'
+
+class PostList(generic.ListView):
+    """
+    This list will display all posts in order by
+    date of posting. It will be paginated by 6 blog
+    posts on each page
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+    paginate_by = 6
