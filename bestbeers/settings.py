70f22bc211fc8d1213e8dba9ed94f9991e9fd0d9
vivignutz <vgnutzmann@gmail.com>
2023-01-14 19:51:27 +0000

    Add Post and Like changes to run server

--- bestbeers/settings.py
+++ bestbeers/settings.py
@@ -67,13 +67,13 @@ SUMMERNOTE_CONFIG = {
     'width': '100%',
 }
 
-#MESSAGE_TAGS = {
-#        messages.DEBUG: 'alert-info',
-#        messages.INFO: 'alert-info',
-#        messages.SUCCESS: 'alert-success',
-#        messages.WARNING: 'alert-warning',
-#        messages.ERROR: 'alert-danger',
-#    }
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
