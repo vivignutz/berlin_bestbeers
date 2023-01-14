539f72549db0843146b79abf33bd8e01ad0cb7b5
vivignutz <vgnutzmann@gmail.com>
2023-01-14 18:39:41 +0000

    Add post add and post update

--- berlin_bestbeers/views.py
+++ berlin_bestbeers/views.py
@@ -84,3 +84,54 @@ class PostLike(LoginRequiredMixin, View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+class PostAdd(LoginRequiredMixin, SuccessMessageMixin, generic.CreateView):
+    """
+    User can add new post and get
+    message back
+    """
+    model = Post
+    template_name = 'post_add.html'
+    form_class = PostForm
+    success_message = 'Post Added'
+
+    def form_valid(self, form):
+        """Validate form after connecting form author to user"""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdate(LoginRequiredMixin, generic.UpdateView):
+    """
+    User can update their posts
+    and get the message tha it was successfull.
+    """
+    model = Post
+    template_name = 'post_update.html'
+    form_class = PostForm
+    success_message = 'Post Updated'
+
+    def form_valid(self, form):
+        """Validate form after connecting form author to user"""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        """Test that logged in user is post author"""
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+            return False
+
+
+def handler404(request, exception):
+    """
+    Custom 404 page
+    """
+    return render(request, '404.html', status=404)
+
+
+def handler500(request):
+    """
+    Custom 500 page
+    """
+    return render(request, '500.html', status=500)
