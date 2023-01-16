90f344d4331b286cc7f2d3f8ae41b81c060a55c6
vivignutz <vgnutzmann@gmail.com>
2023-01-16 13:28:25 +0000

    Changed apps.py

--- berlin-bestbeers/apps.py
+++ berlin-bestbeers/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BerlinBestbeersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'berlin_bestbeers'
+    name = 'berlin-bestbeers'
