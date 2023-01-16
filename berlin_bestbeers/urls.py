f449157f6d1c98ba0157da4bfcbdf67469f024e1
vivignutz <vgnutzmann@gmail.com>
2023-01-16 12:52:02 +0000

    change settings.py and urls.py

--- berlin_bestbeers/urls.py
+++ berlin_bestbeers/urls.py
@@ -5,9 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('berlin_bestbeers', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
-    path('add/,slug:slug>', views.PostAdd.as_view(), name='post_add'),
+    path('add/slug:slug>', views.PostAdd.as_view(), name='post_add'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='blog_post'),
 #    path('<slug:slug>/update/', views.PostUpdate.as_view(), name='post_update'),
 #    path('<slug:slug>/delete/', views.PostDelete.as_view(), name='post_delete'),
