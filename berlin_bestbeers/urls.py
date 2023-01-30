9642f6d6cd76073a2decfeae18a70a5515196fcb
vivignutz <vgnutzmann@gmail.com>
2023-01-30 11:50:57 +0000

    Add button to update and delet post as an user

--- berlin_bestbeers/urls.py
+++ berlin_bestbeers/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('add_post/', AddPost.as_view(), name='add_post'),
     path('post_detail/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', PostLike.as_view(), name='post_like'),
-    path('update_post/edit/<slug:slug>/', views.PostUpdate.as_view(), name='update_post'),
-    path('delete/<slug:slug>/remove', views.PostDelete.as_view(), name='delete_post'),
+    path('update_post/edit/<slug:slug>/', PostUpdate.as_view(), name='update_post'),
+    path('delete/<slug:slug>/remove', PostDelete.as_view(), name='delete_post'),
 #    path('<slug:slug>/comment-update/<int:pk>', views.CommentUpdate.as_view(), name='comment_update'),
 #    path('ModelForm/', views.forms_ModelForm(), name='form_Modelform'),
 ]
