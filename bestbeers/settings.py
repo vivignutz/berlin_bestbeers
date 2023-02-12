943bed4e6bfe941f5eeff09b50cfe0bfd8686873
vivignutz <vgnutzmann@gmail.com>
2023-02-12 11:58:37 +0000

    Add django-extensions

--- bestbeers/settings.py
+++ bestbeers/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
     'django_summernote',
+    "django_extensions",
     'crispy_forms',
     'berlin_bestbeers',
 ]
@@ -65,12 +66,12 @@ SUMMERNOTE_CONFIG = {
 }
 
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-info',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
-    }
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
